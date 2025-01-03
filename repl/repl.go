10a1af313cca71d152e7a00025a32079a0d5cf2b
Kaan Kahveci <kaankahveci29@outlook.com>
2025-01-03 21:08:19 +0300

    Fix typo in REPL

--- repl/repl.go
+++ repl/repl.go
@@ -54,7 +54,7 @@ func Start(in io.Reader, out io.Writer) {
 
 func printParserErrors(out io.Writer, errors []string) {
 	io.WriteString(out, MONKEY_FACE)
-	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, "Whoops! We ran into some monkey business here!\n")
 	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
