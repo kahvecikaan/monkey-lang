63e75e013999e3abe5ab7bcc7d06fb5b40babac1
Kaan Kahveci <kaankahveci29@outlook.com>
2025-01-07 11:40:39 +0300

    Modify REPL to work with env

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/kahvecikaan/monkey-lang/evaluator"
 	"github.com/kahvecikaan/monkey-lang/lexer"
+	"github.com/kahvecikaan/monkey-lang/object"
 	"github.com/kahvecikaan/monkey-lang/parser"
 	"io"
 )
@@ -26,6 +27,7 @@ const MONKEY_FACE = `            __,__
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -44,7 +46,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
